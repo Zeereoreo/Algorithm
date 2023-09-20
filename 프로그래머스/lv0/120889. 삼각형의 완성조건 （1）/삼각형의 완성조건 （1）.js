ad73caacf3f621f1796047fab6a4e4502dfef02c
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2023-09-20 10:02:00 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -3,3 +3,8 @@ function solution(sides) {
     if(answer[0]+answer[1] <= answer[2]) return 2
     else return 1
 }
+// sort 내림차순 만들고 1,2,3 변 모두 정해서 구하기
+// 삼항연사자 사용도 자주 하자 answer[0]+answer[1] <= answer[2] ? 2 : 1
+// 구조분해 할당
+// [long , a , b] = sides.sort((a,b)=>b-a);
+// long < a + b ? 1 : 2
