b6a6b97520bf38b8d5a621e56f4c942e1d7b162d
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2023-10-13 10:01:48 +0900

    [level unrated] Title: 유한소수 판별하기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -0,0 +1,27 @@
+function solution(a, b) {
+  let g = 1;
+  for (let i = 1; i <= b; i++) {
+      if (a % i === 0 && b % i === 0) g = i;
+  }
+  b = b / g;
+  while (b % 2 === 0) b = b / 2; 
+  while (b % 5 === 0) b = b / 5; 
+  return b === 1 ? 1 : 2;
+}
+
+// function solution(a, b) {
+//     let result = 0;
+//     let g = 1;
+//     for(let i = 0; i <= a ; i++){
+//         if(a%i === 0 && b%i === 0){
+//                 return g = i;
+//         }
+//         for(let j = 0; j<= 소인수.length; j++){
+//             if(g%소인수[j] === 0){
+//                 result = 1;
+//             } else {
+//                 result = 2;
+//             }
+//         }
+//         return result;
+// }
