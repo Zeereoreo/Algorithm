c40cb581046f0c3a634619f1fa0959c8257f7a6b
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2023-11-06 11:22:21 +0900

    [level unrated] Title: 수 조작하기 2, Time: 7.93 ms, Memory: 41.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,10 @@
+function solution(numLog) {
+    let result = '';
+    for(let i = 0; i<numLog.length; i++){
+        if(numLog[i] - numLog[i+1] === 1) result +="s"
+        else if(numLog[i] - numLog[i+1] === -1) result +="w"
+        else if(numLog[i] - numLog[i+1] === 10) result +="a"
+        else if(numLog[i] - numLog[i+1] === -10) result +="d"
+    }
+    return result
+}
