44127112c24c6bae5a98cecc17fe15e83264ec59
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2023-11-14 10:36:11 +0900

    [level unrated] Title: 문자 개수 세기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,11 +1,11 @@
-function solution(m) {
+function solution(my_string) {
     var answer = [];
     let al = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     let a = [];
     a.length = 52;
     a.fill(0);
 
-    m.split("").map((n)=>{
+    my_string.split("").map((n)=>{
         a[al.indexOf(n)]+=1
     })
 
