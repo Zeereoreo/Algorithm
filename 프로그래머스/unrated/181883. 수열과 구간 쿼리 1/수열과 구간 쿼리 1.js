6be379c8e3225a8e236d6d729437446d81241a55
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2023-11-17 11:54:03 +0900

    [level unrated] Title: 수열과 구간 쿼리 1, Time: 5.33 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,7 +1,17 @@
 function solution(arr, queries) {
-    for(let query of queries) {
-        for(let i = query[0]; i<=query[1]; i++)
-            arr[i] += 1;
+    // for(let query of queries) {
+    //     for(let i = query[0]; i<=query[1]; i++)
+    //         arr[i] += 1;
+    // }
+    // return arr;
+
+    for(let i=0; i<queries.length; i++){
+        for(let j=0; j<arr.length; j++){
+            if(queries[i][0]<=j&& j<=queries[i][1]){
+                arr[j]++;
+            }
+        }
     }
-    return arr;
+    return arr
+ 
 }
