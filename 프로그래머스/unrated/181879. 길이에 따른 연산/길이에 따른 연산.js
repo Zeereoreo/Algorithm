031ec69a6fd56fc6fa015dc9fcbdd448a8b74317
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2023-11-20 11:09:31 +0900

    [level unrated] Title: 길이에 따른 연산, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+    if (num_list.length >= 11) {
+
+        return num_list.reduce((acc, cur) => acc + cur, 0);
+    } else {
+
+        return num_list.reduce((acc, cur) => acc * cur, 1);
+    }
+}
