922c520aa4027ac18ec3317ca744669021e88383
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2023-11-22 11:01:24 +0900

    [level unrated] Title: 공백으로 구분하기 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,5 +1,6 @@
 function solution(my_string) {
     let a = my_string.split(" ")
     let b = a.filter((el) => el.replace(" "))
-    return b
+    return b;
+    // my_string.split(" ").filter(v=>v) 
 }
