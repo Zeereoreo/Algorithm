b693d0ddbdee4378559bd3a48a0828943e9c8e8b
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-01-18 11:15:27 +0900

    [level 2] Title: 올바른 괄호, Time: 2.59 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,15 @@
+function solution(s){
+    let stack = 0;
+    for(let i = 0; i<s.length; i++){
+        if(s[i] === "("){
+            stack ++;
+        }else {
+            stack --;
+        }
+        if(stack < 0){
+            return false
+        }
+    }
+
+    return stack === 0 ? true : false;
+}
