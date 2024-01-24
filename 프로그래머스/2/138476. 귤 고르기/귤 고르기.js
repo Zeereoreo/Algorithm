d568ed20c9164c85d18e26c1c9386859681600a9
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-01-24 11:17:34 +0900

    [level 2] Title: 귤 고르기, Time: 38.82 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,26 @@
+function solution(k, tangerine) {
+    let answer = 0;
+    // k = another.length
+    // k = 4
+    // [1,2,2,3,3,4,5,5] 1의개수 = 1 2의개수 = 2 3의개수 = 2 4의개수 = 1 5의개수 = 2
+    // 내림차순 => {2 : 2 , 3 : 2 , 5 : 2 , 1 : 1 4 : 1}
+    // 박스에 담는다 => [2,2,3,3] 최솟값 = 2
+    // [2,2,3,3]
+    // [2,2,5,5]
+    // [3,3,5,5]
+    let map = new Map()
+    for(let i = 0; i<tangerine.length; i++){
+        const target = map.get(tangerine[i])
+            map.set(tangerine[i], target ? target + 1 : 1);
+        }
+    let arr = [...map].sort((a,b) => b[1] - a[1])
+    
+    for(let i = 0; i<arr.length; i++){
+        if(k > 0){
+        k -= arr[i][1]
+        answer++
+        }
+    }
+   
+    return answer;
+    }
