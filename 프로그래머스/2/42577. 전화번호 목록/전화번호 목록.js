1929e06bff2e76b0eef95cdb57420de9c1e7f80b
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-01-31 12:01:04 +0900

    [level 2] Title: 전화번호 목록, Time: 106.35 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,12 +1,4 @@
 function solution(phone_book) {
-     phone_book.sort(); 
-
-  for (let i = 0; i < phone_book.length - 1; i++) {
-    
-    if (phone_book[i] === phone_book[i + 1].substring(0, phone_book[i].length)) {
-      return false;
-    }
-  }
-
-  return true; 
+    phone_book.sort();
+    return !phone_book.some((v, i) => phone_book[i+1]?.indexOf(v) === 0);
 }
