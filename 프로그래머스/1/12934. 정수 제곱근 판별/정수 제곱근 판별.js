6c0f376a2024b4a4cd83aca4c1b1d80204035615
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-02-08 11:11:21 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,14 +1,5 @@
 function solution(n) {
-    // var answer = 0;
-    // for(let i = 1; i<=n; i++){
-    //     if(i*i === n){
-    //         answer = (i+1)*(i+1)
-    //     }
-    // }
-    if(Math.sqrt(n)%1 === 0){
-        return (Math.sqrt(n)+1)*(Math.sqrt(n)+1)
-    } else {
-        return -1
-    }
-    // return answer;
+    
+    let N = Math.sqrt(n)
+    return Number.isInteger(N) ? (N+1)*(N+1) : -1
 }
