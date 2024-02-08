1d84607ae7090def2823768455472c1eab575d7b
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-02-08 10:37:19 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,6 +1,9 @@
 function solution(arr) {
-    // var answer = 0;
-    return arr.reduce((a,c) => a+c)/arr.length
-    
-    // return answer;
+    var answer = 0;
+    let acc = 0;
+    for(let i = 0; i<arr.length; i++){
+        acc += arr[i]
+        answer = acc/arr.length;
+    }
+    return answer;
 }
