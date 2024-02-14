dbd81fada3fa3703491aa9b2c69805e96627e391
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-02-14 10:48:18 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,15 +1,16 @@
 function solution(num) {
     let i = 0;
-    while(num !== 1){
+    while(num > 1){
         if(num%2 === 0) {
-            num = num/2
             i++
+            num = num/2
         }
         else {
-            num = num*3+1
             i++
+            num = (num *3) + 1
         }
+        
+        if(i >= 500) return -1
     }
-    if(i>=500) return -1
-    else return i;
+    return i;
 }
