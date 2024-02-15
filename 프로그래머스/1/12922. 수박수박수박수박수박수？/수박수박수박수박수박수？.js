c63da3baed50a6d1768eeaca8cbda82a93101367
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-02-15 12:00:01 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,7 +1,8 @@
 function solution(n) {
     var answer = '';
-    for(let i = 1; i<=n; i++){
-        if(i%2 !== 0) answer+="수"
+    
+    for(let i = 0; i<n; i++){
+        if(i%2 === 0) answer+="수"
         else answer+="박"
     }
     return answer;
