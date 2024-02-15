60e3ca430934896ee8ce60a17cb98e86b7344c16
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-02-15 10:44:03 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,9 +1,10 @@
 function solution(numbers) {
     var answer = 0;
     
-    let a = [1,2,3,4,5,6,7,8,9,0]
-    for(let i = 0; i<a.length; i++){
-        if(!numbers.includes(a[i])) answer+=a[i]
+    for(let i = 0; i<=9; i++){
+        if(!numbers.includes(i)){
+            answer += i
+        }
     }
     return answer;
 }
