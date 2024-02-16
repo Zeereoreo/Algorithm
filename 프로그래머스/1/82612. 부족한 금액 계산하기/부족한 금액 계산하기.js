fbb4f6979243e8f1998988ea70273170748f6c05
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-02-16 11:28:06 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,10 +1,8 @@
 function solution(price, money, count) {
     var answer = 0;
-    let total = 0;
-    for(let i = 1; i <= count; i++){
-        total += price * i
+    let a = 0;
+    for(let i = 1; i<=count; i++){
+        a += price * i
     }
-    answer = total - money
-    if(answer < 0) return 0
-    return answer;
+    return a <= money ? 0 : a - money;
 }
