29bed1a713e8076db6dea799e9baf802ecaa5b1d
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-02-19 10:45:38 +0900

    [level 1] Title: 행렬의 덧셈, Time: 9.56 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,11 @@
 function solution(arr1, arr2) {
-    var answer = [[]];
+    var answer = [];
     for(let i = 0; i<arr1.length; i++){
-        answer[i] = [];
+        let arr = [];
         for(let j = 0; j<arr1[i].length; j++){
-            answer[i].push(arr1[i][j] + arr2[i][j])
+            arr.push(arr1[i][j] + arr2[i][j]);
         }
+         answer.push(arr)
     }
-    
     return answer;
 }
