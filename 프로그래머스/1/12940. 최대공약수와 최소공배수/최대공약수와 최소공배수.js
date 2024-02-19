89a4544ebc609b4f4151dca7d4b239e60730b329
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-02-19 10:53:17 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 4.65 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,17 +1,17 @@
 function solution(n, m) {
-    var answer = [];
-    let min = 1;
-    let max = 1;
+    let gcd = 1;
+    let lcm = 1;
     for(let i = 2; i<=Math.min(n,m); i++){
         if(n%i === 0 && m%i === 0){
-            max = i
+            gcd = i;
         }
     }
+    
     while(true){
-        if(min%n === 0 && min%m === 0){
+        if((lcm%n === 0) && (lcm%m === 0)) {
             break;
         }
-        min++
+        lcm++;
     }
-    return [max,min];
+    return [gcd,lcm];
 }
