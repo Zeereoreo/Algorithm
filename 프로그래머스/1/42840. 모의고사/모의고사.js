c6b9d18db9afaa051f4bc47c60d230d61ff28c02
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-10-01 20:41:27 +0900

    [level 1] Title: 모의고사, Time: 3.69 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,26 +1,21 @@
 function solution(answers) {
     var answer = [];
-    let a = 0;
-    let b = 0;
-    let c = 0;
-    let first = [1,2,3,4,5];
-    let second = [2,1,2,3,2,4,2,5];
-    let third = [3,3,1,1,2,2,4,4,5,5];
-    
-    for (let i = 0; i<answers.length; i++){
-        if(answers[i] === first[i%first.length]) a++
-        if(answers[i] === second[i%second.length]) b++
-        if(answers[i] === third[i%third.length]) c++
+    let a = [1,2,3,4,5]
+    let b = [2,1,2,3,2,4,2,5]
+    let c = [3,3,1,1,2,2,4,4,5,5]
+    let count = [0,0,0];
+ 
+    for(let i = 0; i<answers.length; i++){
+        if(answers[i] === a[i%a.length]) count[0]++
+        if(answers[i] === b[i%b.length]) count[1]++
+        if(answers[i] === c[i%c.length]) count[2]++
     }
     
-    let max = Math.max(a,b,c);
-    
-    if(a === max) answer.push(1);
-    if(b === max) answer.push(2);
-    if(c === max) answer.push(3);
-    
+    let max = Math.max(...count)
+
+    for(let i = 0; i<count.length; i++){
+        if(count[i] === max) answer.push(i+1)
+    }
     
     return answer;
 }
-    
-   
