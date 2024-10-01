08caf215cdda2b719882dce5840c2383a45d4e91
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-10-01 20:20:52 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.56 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -2,11 +2,11 @@ function solution(numbers) {
     var answer = [];
     for(let i = 0; i<numbers.length; i++){
         for(let j = i+1; j<numbers.length; j++){
-        let a = numbers[i]
-        let b = numbers[j]
-        if(!answer.includes(a+b)) answer.push(a+b)
+            answer.push(numbers[i]+numbers[j])
         }
-    
     }
-    return answer.sort((a,b) => a-b);
+    
+    answer = [...new Set(answer)]
+    
+    return  answer.sort((a,b) => a-b);
 }
