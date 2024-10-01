ceda670172e59b73deba2bd087131857a67e3962
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-10-01 20:58:34 +0900

    [level 2] Title: 행렬의 곱셈, Time: 7.45 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,23 +1,22 @@
 function solution(arr1, arr2) {
-    var answer = [];
-//     [[1, 4],            [[3, 3],
-//      [3, 2],            [3, 3]]
-//      [4, 1]]
+    var answer = [[]];
+    let row1 = arr1.length;
+    let col1 = arr1[0].length;
+    let row2 = arr2.length;
+    let col2 = arr2[0].length;
     
-//     [[15, 15],
-//      [15, 15],
-//      [15, 15]]
-    for(let i = 0; i<arr1.length; i++){
-        let result = [];
-        // 0 1 2 , 0 1 3+12 , 3+12 , 9+6 , 9+6 , 12+3, 12+3
-        for(let j = 0; j<arr2[0].length; j++){
-            let el = 0
-            for(let k = 0; k<arr2.length; k++){
-                el += arr1[i][k] * arr2[k][j];
-            }
-            result.push(el)
-        }
-        answer.push(result)
+    const arr = []
+    for(let i = 0; i<row1; i++){
+        arr.push(new Array(col2).fill(0))
     }
-    return answer;
+
+    for(let i = 0; i<row1; i++){
+        for(let j = 0; j<col2; j++){
+           for(let k = 0; k<col1; k++){
+               arr[i][j] += arr1[i][k] * arr2[k][j]
+           } 
+            
+        }
+    }
+    return arr;
 }
