b113dc8961a5e284c5a3ad2bc2f01d6ef5f9ed14
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-10-24 22:43:52 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 3.59 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,27 +1,28 @@
 function solution(board, moves) {
     var answer = 0;
-    let stack = [];
+     let stack = [];
     for(let i = 0; i<moves.length; i++){
-        let now = moves[i] -1
+        let num = moves[i] - 1;
         for(let j = 0; j<board.length; j++){
-            if(board[j][now] != 0){
-                if(stack[stack.length -1] === board[j][now]){
+            if(board[j][num] !== 0){
+                if(stack[stack.length - 1] === board[j][num]){
                     stack.pop();
                     answer += 2;
-                }else {
-                stack.push(board[j][now])
+                } else {
+                    stack.push(board[j][num])
+                }
+                board[j][num] = 0;
+                break;
             }
-            board[j][now] = 0;
-            break;
-                
-            } 
         }
+       
     }
     return answer;
+  
 }
 
-// [0,0,0,0,0]
-// [0,0,1,0,3]
-// [0,2,5,0,1]          move    = [1,5,3,5,1,2,1,4]
-// [4,2,4,4,2]          result  = [4,3,1,1,3,2,0,4]
-// [3,5,1,3,1]         
+// [[0,0,0,0,0],
+//  [0,0,1,0,3],
+//  [0,2,5,0,1],
+//  [4,2,4,4,2],
+//  [3,5,1,3,1]]
