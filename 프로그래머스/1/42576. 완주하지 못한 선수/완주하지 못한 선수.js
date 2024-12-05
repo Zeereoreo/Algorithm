561ff0a01b6b524d3aa2a4edd0572719da3dcaa2
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-12-05 10:48:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 60.16 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,9 +1,22 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
-    for(var i=0;i<participant.length;i++){
-        if(participant[i] !== completion[i]){
-            return participant[i];
+    const obj = {};
+    
+    for(const p of participant){
+        if(obj[p]){
+            obj[p] += 1
+        }else{
+            obj[p]=1
         }
     }
+    
+    for(const c of completion){
+        obj[c] -=1;
+    }
+    
+    for(const key in obj){
+        if(obj[key] > 0){
+            return key
+        }
+    }
+    
 }
