05226d4bbdf965c5effe6c81b14ba34b09bc5cda
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-12-06 10:25:00 +0900

    [level 2] Title: 오픈채팅방, Time: 294.33 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,23 @@
+function solution(record) {
+    var answer = [];
+    const uid = {};
+    
+    for(line in record){
+        const cmd = record[line].split(" ");
+        if(cmd[0] != "Leave"){
+            uid[cmd[1]] = cmd[2]
+        }
+        
+    }
+    
+    for(line in record){
+        const cmd = record[line].split(" ");
+        if(cmd[0] == "Enter"){
+            answer.push(uid[cmd[1]]+"님이 들어왔습니다.")
+        }else if(cmd[0] == "Leave"){
+            answer.push(uid[cmd[1]]+"님이 나갔습니다.")
+        }
+    }
+
+    return answer;
+}
