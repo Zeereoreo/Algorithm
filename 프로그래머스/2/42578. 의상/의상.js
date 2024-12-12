0dfd22f16532df0c906ab008e516669807df2dc3
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-12-12 23:32:59 +0900

    [level 2] Title: 의상, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,23 +1,17 @@
-// function solution(clothes) {
-//     var answer = 0;
-//     let map = new Map()
-//     for(let i = 0; i<clothes.length; i++){
-//         map.set(map.get(clothes[i][1]), map.get(clothes[i][0] || 0) + 1)
-//         console.log(clothes[i][1])
-
-//     }
-//     return answer;
-// }
-
 function solution(clothes) {
-    const map = new Map()
-    for (const [name,type] of clothes){
-        if (map.get(type)) map.set(type,map.get(type)+1)
-        else map.set(type,1)
+    var answer = 1;
+    let map = new Map()
+    for(let i = 0; i<clothes.length; i++){
+        map.has(clothes[i][1]) ?
+            map.set(clothes[i][1], map.get(clothes[i][1]) +1) :
+            map.set(clothes[i][1],1)
+
     }
-    let ret = 1
-    for (const [key,val] of map){
-        ret *= val+1
+    for(let [k,j] of map){
+
+        answer *= j+1
     }
-    return ret-1
+    return answer-1;
 }
+
+
