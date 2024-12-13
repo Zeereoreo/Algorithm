bfee0bdaa5f78db74465a9704de74f3357300970
Zeereoreo <120079336+Zeereoreo@users.noreply.github.com>
2024-12-13 17:32:30 +0900

    [level 2] Title: 예상 대진표, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,14 +1,9 @@
 function solution(n,a,b){
     var answer = 0;
-    while(a !== b){
+    while(a!==b){
         a = Math.ceil(a/2)
         b = Math.ceil(b/2)
         answer++
-        // a = 4 b = 7
-        // a = 2 b = 4 answer = 1
-        // a = 1 b = 2 answer = 2
-        // a = 1 b = 1 answer = 3
     }
-
     return answer;
 }
